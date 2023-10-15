679d17acb9568724491beeafe3f6a965a5dcaa44
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2023-10-15 09:42:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching (drivers, name) {
+    return drivers.filter (driver =>
+        driver.toLowerCase() === name.toLowerCase()
+        );
+}
+
+const drivers = [
+    'Bobby','Sammy','Sally','Annette','Sarah','Bobby'
+]
+
+function fuzzyMatch (drivers, letters) {
+    return drivers.filter( driver =>
+        driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+}
+
+const driverObjects =[
+    {name: 'Bobby', hometown: 'Pittsburgh'},
+    {name: 'Sammy', hometown: 'New York'},
+    {name: 'Sally', hometown: 'Cleveland'},
+    {name: 'Annette', hometown: 'Los Angeles'},
+    {name: 'Bobby', hometown: 'Tampa Bay'},
+]
+
+function matchName (driverObjects, name) {
+    return driverObjects.filter(driver =>
+        driver.name.toLowerCase () === name.toLowerCase ()
+        );
+}
